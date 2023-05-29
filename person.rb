38eeed5925bad4516992ce3aa04d69de4fb6efa4
david30-maker <davidokpah396@gmail.com>
2023-05-29 22:23:12 +0100

    linter error

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 class Person
      attr_reader :id, :name, :age
 
-    def initialize(age, parent_permission = true, name = "Unknown")
+    def initialize(age, parent_permission = true, name = 'Unknown')
         @id = generated_id
         @name = name
         @age = age
