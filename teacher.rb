b83d6a2b4443e3c0ff6a5836e3cc0895229427c7
david30-maker <davidokpah396@gmail.com>
2023-05-29 20:17:23 +0100

    create teacher class

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+require "./person.rb"
+
+class Teacher < Person
+    def initialize(age, parent_permission = true, name = "Unknown", specialization)
+        super(age, parent_permission, name)
+        @specialization = specialization
+    end
+
+    def can_use_service?
+        true
+    end
+end
