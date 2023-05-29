43694db2222108d67546e6e480cfa6351db88aca
david30-maker <davidokpah396@gmail.com>
2023-05-29 22:26:51 +0100

    fix error

--- person.rb
+++ person.rb
@@ -1,33 +1,33 @@
 class Person
-     attr_reader :id, :name, :age
-
+    attr_reader :id, :name, :age
+  
     def initialize(age, parent_permission = true, name = 'Unknown')
-        @id = generated_id
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
+      @id = generate_id
+      @name = name
+      @age = age
+      @parent_permission = parent_permission
     end
-
+  
     def name=(name)
-        @name = name
+      @name = name
     end
-
+  
     def age=(age)
-        @age = age
+      @age = age
     end
-
-    def can_use_service?
-        of_age? || @parent_permission
+  
+    def can_use_services?
+      of_age? || @parent_permission
     end
-
+  
     private
-
+  
     def of_age?
-        @age >= 18
+      @age >= 18
     end
-
-    def generated_id
-        rand(1000..9999)
+  
+    def generate_id
+      rand(1000..9999)
     end
-
-end
+  end
+  
