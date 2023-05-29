9f384c2bc236b12f9675ada7b0138d8d35d0914f
david30-maker <davidokpah396@gmail.com>
2023-05-29 20:06:47 +0100

    create class and method for person class

---
+++ person.rb
@@ -0,0 +1,33 @@
+class Person
+    attr_reader :id, :name, :age
+
+    def initialize(age, parent_permission = true, name = "Unknown")
+        @id = generated_id
+        @name = name
+        @age = age
+        @parent_permission = parent_permission
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def age=(age)
+        @age = age
+    end
+
+    def can_use_service?
+        of_age? || @parent_permission
+    end
+
+    private
+
+    def of_age?
+        @age >= 18
+    end
+
+    def generated_id
+        rand(1000..9999)
+    end
+
+end
